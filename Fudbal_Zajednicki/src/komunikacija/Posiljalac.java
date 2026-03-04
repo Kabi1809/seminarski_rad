f2780e9bca11f63c58c35c71351dea507379e44d
Kabi1809 <kablar.aleksa04@gmail.com>
2026-03-04 13:49:41 +0100

    dodao komunikaciju

---
+++ Fudbal_Zajednicki/src/komunikacija/Posiljalac.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package komunikacija;
+
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+
+/**
+ *
+ * @author Alexa
+ */
+public class Posiljalac {
+    private Socket socket;
+    public Posiljalac(Socket socket) {
+        this.socket = socket;
+    }
+    public void posalji(Object obj){
+        try {
+            ObjectOutputStream out=new ObjectOutputStream(socket.getOutputStream());
+            out.writeObject(obj);
+            out.flush();
+             } catch (Exception ex) {
+                ex.printStackTrace();
+        }
+                
+        }
+    
+}
